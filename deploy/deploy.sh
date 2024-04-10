06ca3b30a1a477925ab739d92241145133db597b
artur <artur.lamaka@gmail.com>
2024-04-10 12:13:07 +0200

    AC-72 Deploy webui

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,4 +2,8 @@ scp -r src root@aicovers:~/AICoverGen
 scp requirements.txt root@aicovers:~/AICoverGen/requirements.txt
 scp deploy/aicovers-api.nginx.conf root@aicovers:/etc/nginx/sites-enabled/aicovers-api
 scp deploy/aicovers-static-files.nginx.conf root@aicovers:/etc/nginx/sites-enabled/aicovers-static-files
+scp deploy/aicovers-api.supervisor.conf root@aicovers:/etc/supervisor/conf.d/aicovers_api.conf
+scp deploy/aicovers-worker.supervisor.conf root@aicovers:/etc/supervisor/conf.d/aicovers_worker.conf
+scp deploy/aicovers-webui.supervisor.conf root@aicovers:/etc/supervisor/conf.d/aicovers_webui.conf
+
 ssh root@aicovers "service supervisor restart"
